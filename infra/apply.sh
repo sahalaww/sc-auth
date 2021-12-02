ad2bd003c0804debf4a09d3832a09d1fc5b99916
Sahala <sahala88@gmail.com>
2021-12-02 13:13:22 +0700

    update script deployment (#4)

--- infra/apply.sh
+++ infra/apply.sh
@@ -3,3 +3,4 @@ kubectl apply -f sc-auth-namespace.yaml
 kubectl apply -f sc-auth-secret.yaml
 kubectl apply -f sc-auth-deployment.yaml
 kubectl apply -f sc-auth-service.yaml
+kubectl apply -f sc-auth-ingress.yaml
